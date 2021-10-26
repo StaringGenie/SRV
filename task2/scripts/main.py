78401f385f51a24de0d2ca68c4d95b441b2c3f9e
StaringGenie <danil090598@gmail.com>
2021-10-27 00:21:01 +0300

    feat: Added occupancy grid

--- task2/scripts/main.py
+++ task2/scripts/main.py
@@ -3,13 +3,18 @@
 import rospy
 import math
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import OccupancyGrid
 
 publisher_old = rospy.Publisher('/laser_scan_old', LaserScan, queue_size = 10)
 publisher_new = rospy.Publisher('/laser_scan_new', LaserScan, queue_size = 10)
+pub_grid = rospy.Publisher('/occupancy_grid', OccupancyGrid, queue_size = 10)
 
 STEPS = 3
-MAX_DISTANCE = 0.05
+MAX_DISTANCE = 0.08
 MAX_FLOAT = 9999
+
+DIMENSIONS = 30
+RESOLUTION = 0.1
 
 
 def distance(p1, p2):
@@ -21,11 +26,69 @@ def coordinates(r, theta):
 def angle(angle_min, angle_increment, position):
     return angle_min + angle_increment * position
 
+def from_polar_to_cart(r, theta):
+    x = r * math.cos(theta)
+    y = r * math.sin(theta)
+    return x,y    
+
 def callback(msg):
     publisher_old.publish(msg)
+
     msg.ranges = remove_noise(msg.ranges, msg.angle_min, msg.angle_increment)
     publisher_new.publish(msg)
 
+    pub_grid.publish(get_grid_msg(msg.ranges, msg.angle_min, msg.angle_increment))    
+
+
+def init_grid():
+    og_msg = OccupancyGrid()
+    width = int(DIMENSIONS / RESOLUTION)
+    height = int(DIMENSIONS / RESOLUTION)
+    og_msg.header.frame_id = 'base_link'
+    og_msg.info.width = width
+    og_msg.info.height = height
+    og_msg.info.resolution = RESOLUTION
+    og_msg.info.origin.position.x = -DIMENSIONS / 2.0
+    og_msg.info.origin.position.y = -DIMENSIONS / 2.0
+    og_msg.data = [-1] * (width * height)
+    return og_msg
+
+def cell(x, y):
+    OFFSET_X = 0.25
+    OFFSET_Y = -0.025
+
+    cell_x = round((x + OFFSET_X + DIMENSIONS/2) / RESOLUTION) 
+    cell_y = round((y + OFFSET_Y + DIMENSIONS/2) / RESOLUTION)
+
+    return cell_x, cell_y
+
+def cast_ray(grid, dist, theta):
+    STEP = RESOLUTION / 2.0
+    STEPS = dist / STEP
+    for i in range(0, int(STEPS)):
+        r = dist / STEPS * i
+        x, y = from_polar_to_cart(r, theta)
+        cell_x, cell_y = cell(x, y)
+        index = cell_x + cell_y * int(DIMENSIONS / RESOLUTION)
+        if grid[index] == -1:
+            grid[index] = 0
+
+def get_grid_msg(ranges, angle_min, angle_step):
+
+    occupancy_grid_msg = init_grid()
+
+    for i, distance in enumerate(ranges):
+        if distance > MAX_FLOAT-1.0:
+            continue
+        angle = angle_min+angle_step*i
+
+        cast_ray(occupancy_grid_msg.data, distance, angle)
+
+        coords = from_polar_to_cart(distance, angle)
+        cell_x, cell_y = cell(coords[0], coords[1])
+        occupancy_grid_msg.data[cell_x + cell_y * int(DIMENSIONS / RESOLUTION)] = 100
+
+    return occupancy_grid_msg
 
 def remove_noise(ranges, angle_min, angle_increment):
     result = []
@@ -42,13 +105,13 @@ def remove_noise(ranges, angle_min, angle_increment):
             result.append(MAX_FLOAT)
         else:
             result.append(ranges[i])
-
+          
     for i in range(STEPS):
         result.append(MAX_FLOAT)
 
     return result
 
-rospy.init_node('lr1_normal')
+rospy.init_node('lr2')
 rospy.Subscriber('base_scan', LaserScan, callback)
 rate = rospy.Rate(1)
 
